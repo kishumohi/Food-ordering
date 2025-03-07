b561f6f428170ae969f712218306bad0b2a96fd1
kishan <kishankalavadiya@gmail.com>
2025-03-07 23:32:29 +0530

    05_Frontend_Layout, Header, AppRoutes Setup

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,9 +1,10 @@
 import { Navigate, Route, Routes } from "react-router";
+import Layout from "./layouts/layout";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<span>Home page</span>} />
+      <Route path="/" element={<Layout>HOME PAGE</Layout>} />
       <Route path="/User-profile" element={<span>USER PROFILE PAGE</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
